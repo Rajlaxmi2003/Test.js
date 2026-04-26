785a8abb0e8428d142a5952cfb200700dcfdb77a
Rajlaxmi <rajlaxmik979@gmail.com>
2026-04-26 07:35:28 +0530

    conversion of datatypes

---
+++ 01_basics/02_Conversion_operation.js
@@ -0,0 +1,89 @@
+/*"use strict"
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));  //as a method 
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+OUTPUT=object
+object
+number
+0    */
+
+/*let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));  //as a method 
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+OUTPUT
+undefined
+undefined
+number
+NaN   */
+
+
+/*let score = true
+
+console.log(typeof score);
+console.log(typeof (score));  //as a method 
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+OUTPUT
+boolean
+boolean
+number
+1    */
+
+
+
+/*let isLoggedIn= 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+OUTPUT
+true
+*/
+
+/*let isLoggedIn= ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+OUTPUT
+false
+*/
+
+/*let isLoggedIn= "Sneha"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+OUTPUT
+true
+*/
+
+/*let isLoggedIn= "Sneha"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn );
+console.log(booleanIsLoggedIn);
+OUTPUT
+boolean
+true
+*/
+
+
+/*
+let someNumber= 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+OUTPUT
+33
+*/
+
+
+let someNumber= 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+OUTPUT
+33
+string
