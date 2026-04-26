3b46e600491bb662a23d882706e76dc083e78d73
Rajlaxmi <rajlaxmik979@gmail.com>
2026-04-26 17:36:07 +0530

    Added my Javascript summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,68 @@
+/*const score=100;
+const scoreValue= 100.3;
+const isLoggedIn= true;
+const outsideTemp= null;
+let userEmail;
+const id= Symbol('123');
+const anotherId= Symbol('123');
+console.log(score);
+console.log(scoreValue);
+console.log(isLoggedIn);
+console.log(outsideTemp);
+console.log(userEmail);
+console.log(id===anotherId);
+const bigNumber= 23457585863383939076n;
+console.log(id===anotherId);
+
+OUTPUT
+100
+100.3
+true
+null
+undefined
+false
+false
+
+*/
+
+
+
+const heros=["shaktiman","nagraj","doga"];
+console.log(heros);
+
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+console.log(myObj);
+
+const myFunction= function(){
+    console.log("Hello World");
+}
+myFunction();
+
+
+
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof  userEmail);
+console.log(typeof id);
+console.log(typeof bigNumber);
+
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+/*OUTPUT
+number
+number
+boolean
+object
+undefined
+symbol
+bigint
+object
+object
+function
+*/
