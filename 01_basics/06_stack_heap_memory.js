1178f0ea80f505941d63062af873649144a35e49
Rajlaxmi <rajlaxmik979@gmail.com>
2026-04-26 18:16:57 +0530

    Added another file

---
+++ 01_basics/06_stack_heap_memory.js
@@ -0,0 +1,18 @@
+let myYoutubename="rajlaxmi.com";
+let anotherName= myYoutubename;
+anotherName="sneha.com";
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+let userOne={
+    email:"rajlaxmik89@gmail.com",
+    upi:"user@ybl"
+}
+let userTwo= userOne;
+userTwo.email="hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userOne.upi);
+console.log(userTwo.upi);
