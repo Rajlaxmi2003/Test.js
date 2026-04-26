11c06b0bea763b3f14b52f415abd9b34bde9f2e4
Rajlaxmi <rajlaxmik979@gmail.com>
2026-04-26 06:20:33 +0530

    Added datatypes folder

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=123132
+let accountEmail="rajlaxmi@gmail.com"
+var accountPassword="123456"
+accountCity="Jamshedpur"
+let accountState;
+
+//accountId=22334
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
