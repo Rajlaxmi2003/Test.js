1e366d09426aaa19c64228a6aef60cf0237315d6
Rajlaxmi <rajlaxmik979@gmail.com>
2026-05-17 20:41:57 +0545

    Added new filw about this and call

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,13 @@
+function SetUsername(username){
+    this.username=username
+}
+
+function createUser( username, email, password){
+    SetUsername(username)
+
+        this.email=email
+        this.password=password
+}
+
+const chai= new createUser("chai","chai@g.com","123")
+console.log(chai);
