d5c9f7b2d0e7a19ab4d4829033377755fec7eb06
Rajlaxmi <rajlaxmik979@gmail.com>
2026-05-17 21:28:12 +0545

    added a new file about inheritence

---
+++ 10_classes_and_oop/inheritence.js
@@ -0,0 +1,34 @@
+class User{
+    constructor(username){
+        this.username=username
+    }
+    
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User{    // extends is giving the prperties of user
+    constructor(username,email,password){
+        super(username)
+        this.email=email;
+        this.password=password;
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+
+const chai=new Teacher("chai","chai@g.com","123")
+chai.addCourse()
+chai.logMe()
+
+const masalachai= new User("masalaChai")
+masalachai.logMe()
+console.log(chai instanceof Teacher);
+console.log(chai instanceof User);
+
